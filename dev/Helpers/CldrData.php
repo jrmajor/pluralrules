831cd7186aa57e9b04f8cf90f3b4d40638019036
Jeremiasz Major <jrh.mjr@gmail.com>
2021-11-08 15:47:01 +0100

    fix phpstan errors

--- dev/Helpers/CldrData.php
+++ dev/Helpers/CldrData.php
@@ -4,6 +4,7 @@ namespace Major\PluralRules\Dev\Helpers;
 
 use Psl\Filesystem;
 use Psl\Json;
+use Psl\Type;
 
 final class CldrData
 {
@@ -14,7 +15,15 @@ final class CldrData
     {
         $data = Filesystem\read_file(__DIR__ . '/../../node_modules/cldr-core/supplemental/plurals.json');
 
+        /** @phpstan-ignore-next-line */
         $rules = Json\decode($data)['supplemental']['plurals-type-cardinal'];
+
+        Type\dict(
+            Type\string(),
+            Type\dict(Type\string(), Type\string()),
+        )->assert($rules);
+
+        /** @phpstan-var array<string, array<string, string>> $rules */
 
         unset($rules['root']);
 
